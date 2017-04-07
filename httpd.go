170b0ca0a2d4b69de3f1f31b1eb5057626591dc7
timeloveboy <734991033@qq.com>
2017-04-07 15:52:09 +0800

    ~

--- httpd.go
+++ httpd.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"image"
+	"image/jpeg"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -15,6 +17,29 @@ func savejpg(req *http.Request) {
 	time := req.FormValue("time")
 	bs, _ := ioutil.ReadAll(req.Body)
 	ioutil.WriteFile(time+".jpg", bs, os.ModePerm)
+
+}
+func fiximg(time string) error {
+	fi, err := os.Open(time + ".jpg")
+	if err != nil {
+		return err
+	}
+	defer fi.Close()
+	img, err := jpeg.Decode(fi)
+	if err != nil {
+		return err
+	}
+
+	//去掉黑边
+	newimg := image.RGBA{img.Bounds()}
+	for y := 1; y < img.Bounds().Dy(); y++ {
+		for x := 1; x < img.Bounds().Dx(); x++ {
+			newimg.Set(x, y, img.At(x, y))
+		}
+	}
+
+	jpeg.Encode(fi, newimg, &jpeg.Options{Quality: 100})
+	return nil
 }
 func exec_tesseract(time string) (string, error) {
 	cmd := exec.Command("tesseract", time+".jpg", time+".temp", "-l", "eng", "-psm", "6", "-c", "tessedit_char_whitelist="+charWhitelist)
@@ -37,6 +62,7 @@ func main() {
 	http.HandleFunc("/upload", func(w http.ResponseWriter, req *http.Request) {
 		time := req.FormValue("time")
 		savejpg(req)
+		fiximg(time)
 		result, err := exec_tesseract(time)
 		if err != nil {
 			w.WriteHeader(400)
